992303a00cfed2fff0353914c6ada46650d7635a
SakyubaNoe <398noe@gmail.com>
2023-08-20 22:07:25 +0900

    Add setup scripts

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# update software
+sudo apt update
+sudo apt upgrade -y
+
+# install software
+# build tools
+sudo apt install git build-essential -y
+
+# editor
+# nano
+sudo apt install nano -y
