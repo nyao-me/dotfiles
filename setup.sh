73c4e9df4bc99701ef1ee5d0008bac585198184e
さきゅばのえ <398noe@gmail.com>
2024-02-02 06:32:33 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,9 +11,13 @@ sudo apt-get upgrade -y
 sudo apt install nano -y
 sudo apt-get install git -y
 sudo apt-get install build-essential -y
+# sudo apt-get install snapd -y
 
 # install nodejs
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update
 sudo apt-get install -y nodejs
 
@@ -26,7 +30,7 @@ echo 'export PATH=$N_PREFIX/bin:$PATH' >> $HOME/.bashrc
 
 source ~/.bashrc
 
-n latest
+n lts
 
 echo "Installed node version: $(node -v)"
 echo "Installed npm version: $(npm -v)"
@@ -46,7 +50,6 @@ conda init
 conda config --set auto_activate_base true
 conda update --all -y
 
-
 source ~/.bashrc
 
 rm $HONE/miniconda.sh
@@ -62,3 +65,12 @@ echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.bashrc
 
 source ~/.bashrc
 echo "Installed go version: $(go version)"
+
+# Install aws and gcp
+sudo apt-get install -y apt-transport-https ca-certificates gnupg curl sudo
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install -y google-cloud-cli
+
+echo "Installed gcp-cli"
+echo "done!"
